d6351ba06754089e69a4a6ac82951bf3910fccff
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2024-09-13 22:50:16 -0500

    removed rest controller and service and switched to sprind data rest

--- src/main/java/com/springboot/employee_crud/dao/EmployeeRepository.java
+++ src/main/java/com/springboot/employee_crud/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.springboot.employee_crud.dao;
 
 import com.springboot.employee_crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // that's it ... no need to write any code
 }
